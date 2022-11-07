5499751790f5a7f8764891907a5cc0fab876324b
hyeong-jun-98 <bellehee98@naver.com>
2022-11-07 16:25:13 +0900

    Initial commit

---
+++ src/main/java/com/academy/latteis/LatteIsApplication.java
@@ -0,0 +1,13 @@
+package com.academy.latteis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatteIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatteIsApplication.class, args);
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: SL
+  Date: 2022-11-07
+  Time: 오후 4:03
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    333dsdsdas
+</head>
+<body>
+
+</body>
+</html>
