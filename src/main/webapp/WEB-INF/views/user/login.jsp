292b95c3de8ad79016a4cf79a0d0bd6c6e5eebea
dojuun2 <djp9816@naver.com>
2022-11-16 14:34:55 +0900

    [22.11.16] 자유게시판 게시글 수정, 삭제 로그인 인터셉터 처리

--- src/main/webapp/WEB-INF/views/user/login.jsp
+++ src/main/webapp/WEB-INF/views/user/login.jsp
@@ -138,6 +138,7 @@
     }
 
     const message = '${message}';
+    console.log(message);
     if (message === 'no-login'){
         alert('로그인이 필요한 서비스입니다.');
     }
--- src/test/java/com/academy/latteis/board/repository/BoardMapperTest.java
+++ src/test/java/com/academy/latteis/board/repository/BoardMapperTest.java
@@ -1,6 +1,7 @@
 package com.academy.latteis.board.repository;
 
 import com.academy.latteis.board.domain.Board;
+import com.academy.latteis.board.dto.ValidateUserDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,28 @@ class BoardMapperTest {
             board.setContent("안녕하세용용이" + i);
             mapper.write(board);
         }
+    }
+
+    @Test
+    @DisplayName("")
+    void findAccountTest() {
+        //given
+
+        //when
+        ValidateUserDTO dto = mapper.findUserByBoardNo(333L);
+        //then
+        System.out.println("dto = " + dto);
+    }
+
+    @Test
+    @DisplayName("")
+    void findOneTest() {
+        //given
+
+        //when
+        Board board = mapper.findOne(342L);
+        //then
+        System.out.println("board = " + board);
     }
 
 }
