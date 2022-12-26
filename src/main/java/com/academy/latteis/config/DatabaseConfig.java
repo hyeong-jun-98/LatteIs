739163ae86e0f145e0a65b57f01afeb8b908f32e
hyeong-jun-98 <bellehee98@naver.com>
2022-12-26 14:18:47 +0900

    [22.12.26]    반응형 웹 수정  - HJ

--- src/main/java/com/academy/latteis/config/DatabaseConfig.java
+++ src/main/java/com/academy/latteis/config/DatabaseConfig.java
@@ -12,23 +12,24 @@ import javax.sql.DataSource;
 @Configuration
 @PropertySource("classpath:db_info.properties")
 public class DatabaseConfig {
-//    @Value("${aws.rds_user_name}")
-//    private String username;
-//
-//    @Value("${aws.rds_password}")
-//    private String password;
-//
-//    @Value("${aws.rds_url}")
-//    private String url;
 
-    @Value("${local.db.username}")
+    @Value("${aws.rds_user_name}")
     private String username;
 
-    @Value("${local.db.password}")
+    @Value("${aws.rds_password}")
     private String password;
 
-    @Value("${local.db.url}")
+    @Value("${aws.rds_url}")
     private String url;
+
+//    @Value("${local.db.username}")
+//    private String username;
+//
+//    @Value("${local.db.password}")
+//    private String password;
+//
+//    @Value("${local.db.url}")
+//    private String url;
 
 
     @Bean
